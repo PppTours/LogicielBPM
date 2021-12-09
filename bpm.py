3a02663a3b4a9e0adb613ce48446df58204e47dc
thomasciron <thomas.ciron@etu.univ-tours.fr>
2021-12-09 14:45:27 +0100

    Add: init_plage_bpm

--- bpm.py
+++ bpm.py
@@ -28,10 +28,33 @@ def aubio_object(filepath, music_length = 4 * 60 * 1000):
     """
     return aubio.source(filepath, 0, music_length)
 
+def init_plage_bpm(bpm_debut, bpm_fin, pas):
+    """
+    Crée une plage allant de bpm_debut à bpm_fin en allant de pas en pas.
+    """
+    plage_bpm = [bpm_debut]
+    while(plage_bpm[-1] < bpm_fin):
+        plage_bpm.append(plage_bpm[-1]+pas)
+    return plage_bpm
+        
+
+def calcul_bpm(son, fs, pas_fin = 0.05, bpm_debut = 85, bpm_fin = 160, pas_grossier = 1):
+    """
+    Calcule le BPM.
+    """
+    pas = pas_grossier
+    trouve_grossier = False
+    trouve_bpm = False
+    while not(trouve_bpm):
+        # Plage de BPM à parcourir :
+        plage_bpm = init_plage_bpm(bpm_debut, bpm_fin, pas)
+        print("Plage de BPM : ",plage_bpm)
+        exit()
 
 # TODO : demander à l'utilisateur de choisir un fichier ou prendre en compte le paramètre pris en compte
 if __name__=="__main__":
-    print_audio_track("Never Gonna Give You Up.mp3")
+    audio_file = "Never Gonna Give You Up.mp3"
+    calcul_bpm(aubio_object(audio_file), 1)
 """
 TODO : calculer le BPM
 Ça marche pas du tout ptdr
