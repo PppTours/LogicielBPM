3754e0da16847f0342aa3960f705d34857127a2c
thomasciron <thomas.ciron@etu.univ-tours.fr>
2021-12-09 17:37:35 +0100

    Add: calcul_bpm() v2

--- bpm.py
+++ bpm.py
@@ -5,10 +5,6 @@ import aubio
 import stats
 import time
 import librosa
-import numpy
-import IPython.display as ipd
-
-import librosa.display
 
 
 def print_audio_track(audio_file):
@@ -43,6 +39,13 @@ def init_plage_bpm(bpm_debut, bpm_fin, pas):
         plage_bpm.append(plage_bpm[-1]+pas)
     return plage_bpm
         
+def calcul_bpm(audio_file):
+    """
+    Calcule le BPM.
+    """
+    src = aubio_object(audio_file)
+    x2, sr2 = src()
+    return librosa.beat.tempo(x2, sr=sr2)[0]*0.8
 
 def calcul_bpm2(src, fs, pas_fin = 0.05, bpm_debut = 85, bpm_fin = 160, pas_grossier = 1):
     """
@@ -66,49 +69,41 @@ def calcul_bpm2(src, fs, pas_fin = 0.05, bpm_debut = 85, bpm_fin = 160, pas_gros
             extrait2 = son[min(1 + decal, son[-1])]
             print(decal, decalage[i])
         exit()
-
-def calcul_bpm(audio, fs, pas_fin = 0.05, bpm_debut = 85, bpm_fin = 160, pas_grossier = 1):
     """
-    Calcule le BPM.
+    TODO : calculer le BPM
+    Ça marche pas du tout ptdr
+    max = 0
+    k = 0
+    barre = -10
+    print("Calcul en cours...")
+    for i in range(1, len(samples)//2, 240):
+        autocorrelation_tmp = stats.autocorrelation(samples, i)
+        if autocorrelation_tmp > max :
+            max = autocorrelation_tmp
+            k = i
+
+    print()
+    print(k)
+    print(max)
+    """
+
+def compare_bpm(audio1, audio2):
+    """
+    Compare des BPM.
     """
     print("Calcul du BPM en cours . . .")
-    x, sr = librosa.load(audio)
-    src = aubio_object(fs)
-    x2, sr2 = src()
+    x, sr = librosa.load(audio1)
     tempo = librosa.beat.tempo(x, sr=sr)
     time_start = time.time()
-    tempo2 = librosa.beat.tempo(x2, sr=sr2)
-    bpm = tempo2[0]*0.8
+    bpm = calcul_bpm(audio2)
     duree = time.time() - time_start
     print("Vrai BPM : ", tempo[0])
     print("BPM de l'arnaque : ", bpm)
     print("Temps de calcul : " + str(duree) + "s")
 
-    T = len(x2)/float(sr2)
-    seconds_per_beat = 60.0/bpm
-    beat_times = numpy.arange(0, T, seconds_per_beat)
-    clicks = librosa.clicks(beat_times, sr, length=len(x))
-    ipd.Audio(x + clicks, rate=sr)
 
 # TODO : demander à l'utilisateur de choisir un fichier ou prendre en compte le paramètre pris en compte
 if __name__=="__main__":
     audio_file2 = "Never Gonna Give You Up.mp3"
     audio_file = "12.wav"
-    calcul_bpm(audio_file, audio_file2)
-"""
-TODO : calculer le BPM
-Ça marche pas du tout ptdr
-max = 0
-k = 0
-barre = -10
-print("Calcul en cours...")
-for i in range(1, len(samples)//2, 240):
-    autocorrelation_tmp = stats.autocorrelation(samples, i)
-    if autocorrelation_tmp > max :
-        max = autocorrelation_tmp
-        k = i
-
-print()
-print(k)
-print(max)
-"""
+    compare_bpm(audio_file, audio_file2)
